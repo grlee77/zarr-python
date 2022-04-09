70323f07bf051a901c71e7dddb3ec0e9a926377f
Gregory Lee <grlee77@gmail.com>
2022-04-09 18:24:08 -0400

    move zarr StoreV3 implementations to storage_v3.py

--- zarr/_storage/store.py
+++ zarr/_storage/store.py
@@ -1,4 +1,5 @@
 import abc
+import os
 from collections.abc import MutableMapping
 from string import ascii_letters, digits
 from typing import Any, List, Mapping, Optional, Union
@@ -16,6 +17,9 @@ meta_root = 'meta/root/'
 data_root = 'data/root/'
 
 DEFAULT_ZARR_VERSION = 2
+
+v3_enabled = os.environ.get('ZARR_ENABLE_V3', '0').lower()
+v3_enabled = v3_enabled not in ['0', 'false']
 
 
 class BaseStore(MutableMapping):
@@ -261,7 +265,7 @@ class StoreV3(BaseStore):
 
         We'll do this conversion in a few places automatically
         """
-        from zarr.storage import KVStoreV3  # avoid circular import
+        from zarr.storage_v3 import KVStoreV3  # avoid circular import
         if store is None:
             return None
         elif isinstance(store, StoreV3):
